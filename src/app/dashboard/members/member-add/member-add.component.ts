08dee66ea7812b85900b27028801a8a4f8354ac6
Monil Gudhka <gudhkamonil@gmail.com>
2022-04-12 19:16:08 +0530

    updating member add component

--- src/app/dashboard/members/member-add/member-add.component.ts
+++ src/app/dashboard/members/member-add/member-add.component.ts
@@ -1,18 +1,26 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FamilyService } from 'src/app/shared/service/family.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { MembersService } from 'src/app/shared/service/members.service';
 
 @Component({
   selector: 'app-member-add',
   templateUrl: './member-add.component.html'
 })
-export class MemberAddComponent {
-  @ViewChild('memberName') memberName: ElementRef;
+export class MemberAddComponent implements OnInit {
+  createForm: FormGroup;
 
-  constructor(private familyService: FamilyService) { }
+  constructor(private membersService: MembersService) { }
 
-  onAddMember() {
-    this.familyService.createMember(this.memberName.nativeElement.value);
-    this.memberName.nativeElement.value = '';
+  ngOnInit(): void {
+    this.createForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.minLength(3)])
+    });
+  }
+
+  onCreate(formDirective: FormGroupDirective) {
+    this.membersService.create(this.createForm.value.name);
+    this.createForm.reset();
+    formDirective.resetForm();
   }
 
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   exports: [
@@ -15,7 +16,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
