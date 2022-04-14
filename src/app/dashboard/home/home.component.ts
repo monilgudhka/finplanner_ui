eb9f45db14d053d7c9e6c290ca5268efad7e7018
Monil Gudhka <gudhkamonil@gmail.com>
2022-04-14 18:38:29 +0530

    shifting home content into summary

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,31 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Growth } from 'src/app/shared/model/growth.model';
-import { FamilyService } from 'src/app/shared/service/family.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  growth: Growth;
-
-  private familySubscription: Subscription;
-
-  constructor(private familyService: FamilyService) { }
-
-  ngOnInit(): void {
-    this.familySubscription = this.familyService.subscribeFamily(family => {
-      this.growth = family.getGrowth();
-    });
-    const family = this.familyService.getFamily();
-    if (family) {
-      this.growth = family.getGrowth();
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.familySubscription.unsubscribe();
-  }
+export class HomeComponent {
 
 }
---
+++ src/app/dashboard/home/summary/summary.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Growth } from 'src/app/shared/model/growth.model';
+import { FamilyService } from 'src/app/shared/service/family.service';
+
+@Component({
+    selector: 'app-summary',
+    templateUrl: './summary.component.html'
+})
+export class SummaryComponent implements OnInit, OnDestroy {
+    growth: Growth;
+
+    private familySubscription: Subscription;
+
+    constructor(private familyService: FamilyService) { }
+
+    ngOnInit(): void {
+        this.familySubscription = this.familyService.subscribeFamily(family => {
+            this.growth = family.getGrowth();
+        });
+        const family = this.familyService.getFamily();
+        if (family) {
+            this.growth = family.getGrowth();
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.familySubscription.unsubscribe();
+    }
+    
+}
